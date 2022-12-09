0751880d6709686ffaedfa92495ac5e110b9afe2
sanyaoooo <sanyaobox@gmail.com>
2022-12-09 17:04:16 +0800

    Complete header & footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,20 @@
 </script>
 
 <template>
-  <footer>
-    <div class="container">
-        <p>© 2022 人嶼物股份有限公司</p>
+  <footer class="bg-blue text-white">
+    <div class="container py-6 text-center">
+        <div class="mt-5 mb-8 flex gap-5 justify-center">
+            <a href="https://www.facebook.com/goodtogo.tw" target="_blank"><img src="../assets/img/icon_facebook_circle.svg" class="social-media-icon" /></a>
+            <a href="https://www.instagram.com/goodtogo.tw/" target="_blank"><img src="../assets/img/icon_instagram_circle.svg" class="social-media-icon" /></a>
+            <a href="mailto:hi@goodtogo.tw" target="_blank"><img src="../assets/img/icon_mail_circle.svg" class="social-media-icon" /></a>
+        </div>
+        <p class="text-blue-100">© 2022 人嶼物股份有限公司</p>
     </div>
   </footer>
 </template>
 
 <style scoped>
+.social-media-icon {
+    display: inline-block;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,11 +4,61 @@
 
 <template>
   <header>
-    <div class="container">
-        <img src="../assets/logo_goodtogo.svg" class="logo" alt="GoodToGo logo" />
+    <div class="container py-8">
+      <div class="flex justify-between items-center">
+        <img src="../assets/img/logo_goodtogo.svg" class="logo" alt="GoodToGo logo" />
+        <nav class="flex items-center gap-8">
+          <ul class="primary-menu">
+            <li>
+              <router-link to="/service">
+                服務內容<span class="material-symbols-rounded">arrow_drop_down</span>
+              </router-link>
+              <ul class="dropdown-menu">
+                <li><router-link to="/service">循環杯</router-link></li>
+                <li><router-link to="/service">專業清洗</router-link></li>
+              </ul>
+            </li>
+          </ul>
+          <router-link to="/" class="btn btn-bg-yellow">加入會員</router-link>
+        </nav>
+      </div>
     </div>
   </header>
 </template>
 
 <style scoped>
+.logo {
+  max-width: 205px;
+}
+.primary-menu {
+  @apply flex items-center gap-4
+}
+.primary-menu a {
+  @apply px-4 py-4 text-lg block font-medium transition
+}
+.primary-menu a:hover {
+  @apply text-blue
+}
+.primary-menu li {
+  @apply relative
+}
+.primary-menu a .material-symbols-rounded {
+  vertical-align: sub;
+}
+.dropdown-menu {
+  opacity: 0;
+  transform: translateY(-10px);
+  pointer-events: none;
+  transition: all 0.4s ease;
+  @apply absolute right-4 bg-white py-1 text-right
+}
+.primary-menu li:hover .dropdown-menu,
+.primary-menu li.hover .dropdown-menu {
+  opacity: 1;
+  transform: translateY(0px);
+  pointer-events: initial;
+}
+.dropdown-menu a {
+  @apply py-1 text-xs
+}
 </style>
