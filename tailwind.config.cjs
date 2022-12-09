73fffe6f73fe0b7379d0f69a7754815e2bbe03be
sanyaoooo <sanyaobox@gmail.com>
2022-12-09 14:57:23 +0800

    Add colors config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,19 +6,37 @@ module.exports = {
     extend: {},
     container: {
       center: true,
-      paddingLeft: {
+      padding: {
         DEFAULT: '1rem',
         sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        '2xl': '5rem',
       },
-      paddingRight: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+      screens: {
+        sm: '560px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1024px',
+        '2xl': '1280px',
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: {
+        DEFAULT: '#FFFFFF',
+        op: {
+          80: '#FFFFFFCC'
+        },
+      },
+      black: '#1B1E1F',
+      blue: {
+        DEFAULT: '#40B9D8', // 500
+        100: '#D9F1F7',
+        250: '#9FDCEC',
+      },
+      yellow: {
+        DEFAULT: '#FFD84B', // 500
+        700: '#FFC800',
       },
     },
   },
